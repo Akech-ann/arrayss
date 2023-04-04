44df8a628aa2ada82f68ec006491d841cda4af54
Akech-ann <akechannatong@gmail.com>
2023-04-04 08:14:01 +0300

    first commit

---
+++ prac.js
@@ -0,0 +1,16 @@
+let a = [3,34,64,65,7,87];
+let b = a.length-1;
+console.log(a[b]);
+
+let arr = ["green",true,"blue",26,27];
+let arr1 = arr.length-1;
+console.log(arr[arr1]);
+
+
+let mypets = ["cow","snake","bird","dog"];
+let pets = mypets.join();
+console.log(pets);
+//Write a JS script to sort the following array items
+// = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+
+
